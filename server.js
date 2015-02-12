6b22088c6c2971f88c94dd42ac7672cff87c5af0
Adam Magaluk <AdamMagaluK@gmail.com>
2015-02-12 10:41:37 -0500

    Using external npm modulesx

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var zetta = require('zetta');
+var KairosDbCollector = require('zetta-device-data-kairosdb');
+var InfluxDbCollector = require('zetta-device-data-influxdb');
 
-var KairosDbCollector = require('../collection-kairosdb');
 var kairos = new KairosDbCollector({
   host: process.env.KAIROS_HOST,
   port: process.env.KAIROS_PORT || 8080
 });
 
-var InfluxDbCollector = require('../collection-influxdb');
 var influx = new InfluxDbCollector({
   host: process.env.INFLUX_HOST,
   port: process.env.INFLUX_PORT || 8086,
